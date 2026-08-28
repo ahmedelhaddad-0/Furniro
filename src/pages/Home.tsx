4abbd70cde5c070ebf5d69c0cfd713b44cd23a24
ahmed1ibrahim12 <130283956+ahmedelhaddad-0@users.noreply.github.com>
2026-08-28 03:54:54 +0300

    testing images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,24 @@
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+      <img src="https://mamp.one/wp-content/uploads/2024/09/image-resources2.jpg" alt="Home" />
+    </div>
   )
 }
